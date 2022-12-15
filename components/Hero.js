4a23907e74e88aad5cccf3789490d24b62fda43b
Tunay Bektas <what05@web.de>
2022-12-15 16:27:41 +0100

    getStaticprops added the API-KEY added axios

--- components/Hero.js
+++ components/Hero.js
@@ -5,10 +5,16 @@ const Hero = () => {
   return (
     <div className="text-center bg-white pb-10">
       <div className="w-60 mx-auto">
-        <Image src={"/home_cinema.png"} width={200} height={200} responsive />
+        <Image
+          src={"/home_cinema.png"}
+          width={200}
+          height={200}
+          alt="Man watching a movie"
+          responsive
+        />
       </div>
       <h1 className="text-2xl text-gray-700 uppercase font-bold">Next Movie</h1>
-      <p className="text-gray-500">Filme und Serien</p>
+      <p className="text-gray-500">Deine Filme Datenbank!</p>
       <Link href="/kontakt">
         <button className="bg-gray-700 text-white py-3 px-6 rounded text-sm mt-4">
           Kontakt
