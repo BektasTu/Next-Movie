7eaa6a95fc4670f3ac6dd11cf4823ff99e1075d2
Tunay Bektas <what05@web.de>
2022-12-15 11:50:02 +0100

    Kontakt created kontakt.js to navigate through the kontakt button

---
+++ pages/kontakt.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const kontakt = () => {
+  return (
+    <div className="text-center mt-20">
+      <h1 className="text-4xl font-bold">Kontaktieren Sie uns!</h1>
+    </div>
+  );
+};
+
+export default kontakt;
