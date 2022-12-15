4e8aca7abbfa83cabd5c1a398db787a06c3601fb
Tunay Bektas <what05@web.de>
2022-12-15 11:38:42 +0100

    Hero Component created the hero component added an image, text and button linked to contact

---
+++ components/Hero.js
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Hero = () => {
+  return (
+    <div className="text-center bg-white pb-10">
+      <div className="w-60 mx-auto">
+        <Image src={"/home_cinema.png"} width={200} height={200} responsive />
+      </div>
+      <h1 className="text-2xl text-gray-700 uppercase font-bold">Next Movie</h1>
+      <p className="text-gray-500">Filme und Serien</p>
+      <Link href="/kontakt">
+        <button className="bg-gray-700 text-white py-3 px-6 rounded text-sm mt-4">
+          Kontakt
+        </button>
+      </Link>
+    </div>
+  );
+};
+
+export default Hero;
