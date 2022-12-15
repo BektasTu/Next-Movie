6c401e88d5b94302c52031948a1e73361a58d25c
Tunay Bektas <what05@web.de>
2022-12-15 16:58:56 +0100

    fixed an issue

--- components/PopularMovies.js
+++ components/PopularMovies.js
@@ -3,7 +3,7 @@ import MoviesCard from "./MoviesCard";
 const PopularMovies = ({ movies }) => {
   return (
     <div className="bg-gray-700 container max-w-7xl mx-auto pb-10 px-4">
-      <h1 className="text-white text-2xl mt-8 mb-5">What's Popular</h1>
+      <h1 className="text-white text-2xl mt-8 mb-5">Angesagte Filme</h1>
       <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
         {movies.map((movie) => (
           <MoviesCard movie={movie} key={movie.id} />
